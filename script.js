12445300fe2a19c01c0923038f8f1c6b5cbda2a9
React Obsessed <radhoine.thabti@gmail.com>
2024-06-26 14:36:53 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+var colorBox = document.getElementById('color-box');
+    var changeColorBtn = document.getElementById('change-color-btn');
+    
+    document.addEventListener('DOMContentLoaded', function() {
+        
+        function getRandomColor() {
+        const letters = '0123456789ABCDEF';
+        let color = '#';
+        for (let i = 0; i < 6; i++) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
+    }
+
+    changeColorBtn.addEventListener('click', function() {
+        colorBox.style.backgroundColor = getRandomColor();
+    });
+});
